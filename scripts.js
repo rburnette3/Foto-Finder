b1c5d72476ef72d8541fff880fb4fd6261e88655
Ralph Burnette <rgburnette3@gmail.com>
2017-03-29 16:27:30 -0600

    restructure javascript for pic card

--- scripts.js
+++ scripts.js
@@ -5,7 +5,7 @@ var fileImageChooserButton = document.getElementById('choose-file-button');
 var imageTitle = document.getElementById('image_title');
 var userImageCaption = document.getElementById('user_image_caption');
 var userImage = document.getElementById('user_image');
-
+var bottomSection = document.getElementById('bottom_section');
 
 function fileChooser(filepath) {
   return filepath.split('\\').pop();
@@ -14,10 +14,36 @@ function fileChooser(filepath) {
 
 addToAlbumButton.addEventListener('click', function() {
   console.log("I've been clicked");
-  imageTitle.innerText = titleInput.value;
-  userImageCaption.innerText = captionInput.value;
-  userImage.value = "photos/" + fileChooser(fileImageChooserButton.value);
-  console.log(fileChooser(fileImageChooserButton.value));
+  // var title = titleInput.value;
+  // var caption  = captionInput.value;
+  // var chosenPhoto = "photos/" + fileChooser(fileImageChooserButton.value);
+  // console.log(fileChooser(fileImageChooserButton.value));
+  // console.log(chosenPhoto);
 // var imageTitle.innerText = titleInput.value;
 // var userImageCaption.innerText = captionInput.value;
+  addPhotoCard();
 })
+
+
+
+function addPhotoCard() {
+  var newPhotoCard = document.createElement('div');
+  newPhotoCard.className = 'photo-card'
+
+  var title = titleInput.value;
+  var caption  = captionInput.value;
+  var chosenPhoto = "photos/" + fileChooser(fileImageChooserButton.value);
+
+
+  newPhotoCard.innerHTML =
+  '<h5 id="image_title" class="image-title">' + title + '</h5>' +
+  '<img src="' + chosenPhoto + '" alt="User uploaded image." id="user_image" class="user-image"/>' +
+  '<p id="user_image_caption">' + caption + '</p>' +
+  '<div id="card_footer" class="card-footer"><button class="card_buttons" type="button" name="button">' +
+  '<img src="photos/delete.svg"/>' +
+    '</button>' +
+    '<button id="favorite_button" class="card_buttons" type="button" name="button">' +
+    '<img src="photos/favorite.svg"/></button>' +
+  '</div>'
+  bottomSection.appendChild(newPhotoCard);
+}
