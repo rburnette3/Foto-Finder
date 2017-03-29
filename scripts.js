8d6c65059d1936e2cb3e6c162a37de4f2a4562a4
Hector Sanchez <hsanchez7934@gmail.com>
2017-03-29 13:59:45 -0600

    Updated Javascript with file path function.

--- scripts.js
+++ scripts.js
@@ -1,15 +1,23 @@
 var titleInput = document.getElementById('title_input');
 var captionInput = document.getElementById('caption_input');
-var fileImageChooserButton = document.getElementById('choose-file-button');
 var addToAlbumButton = document.getElementById('saveButton');
+var fileImageChooserButton = document.getElementById('choose-file-button');
 var imageTitle = document.getElementById('image_title');
 var userImageCaption = document.getElementById('user_image_caption');
 var userImage = document.getElementById('user_image');
+
+
+function fileChooser(filepath) {
+  return filepath.split('\\').pop();
+};
+
 
 addToAlbumButton.addEventListener('click', function() {
   console.log("I've been clicked");
   imageTitle.innerText = titleInput.value;
   userImageCaption.innerText = captionInput.value;
+  userImage.value = "photos/" + fileChooser(fileImageChooserButton.value);
+  console.log(fileChooser(fileImageChooserButton.value));
 // var imageTitle.innerText = titleInput.value;
 // var userImageCaption.innerText = captionInput.value;
 })
